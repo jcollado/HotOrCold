8ef32ef70341c016d68539dccf77eeee46a98b59
Javier Collado <javier.collado@gmail.com>
2013-10-14 03:51:57 +0200

    Added new game button

--- application.js
+++ application.js
@@ -1,23 +1,66 @@
 $(document).ready(function() {
-    var secretNumber = getRandomInt(0, 100);
-    console.log("secretNumber: " + secretNumber);
+    var maxTemp = 100;
+    var secretNumber = null;
+    var oldTemp = null;
 
-    var oldTemp = 0;
+    // Bind to form submission instead of button click
+    // to take advantage of pattern validation
+    $("form").submit(handleSubmit);
+    $("#new_game").click(initialize);
 
-    $("form").submit(function() {
-        var temp = getGuessTemp(secretNumber);
+    function initialize() {
+        secretNumber = getRandomInt(0, 100);
+        console.log("secretNumber: " + secretNumber);
+        oldTemp = 0;
+
+        var mercury = $("#th-mercury .th-top");
+        mercury.animate({height: tempToHeight(oldTemp)}, "fast");
+
+        $("#feedback p").hide();
+        $("#feedback_waiting").show();
+
+        $("#guess").val("");
+
+        return false;
+    }
+
+    // Returns a random integer between min and max
+    // Using Math.round() will give you a non-uniform distribution!
+    function getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    // Transform temperature to height
+    function tempToHeight(temp) {
+        var maxHeight = 423;
+        return temp * maxHeight / maxTemp;
+    }
+
+    // Get guess temperature
+    function getGuessTemp() {
+        var guess_value = $("#guess").val()
+        console.log("guess: "  + guess_value);
+
+        var temp = 100 - Math.abs(secretNumber - guess_value);
+        console.log("temp: " + temp);
+
+        return temp;
+    }
+
+    function handleSubmit() {
+        var temp = getGuessTemp();
 
         // Textual feedback
         $("#feedback p").hide();
-        if (temp == 100) {
-            $("#guessed").show();
+        if (temp == maxTemp) {
+            $("#feedback_guessed").show();
         }
         else if (temp > oldTemp) {
-            $("#hotter").show();
+            $("#feedback_hotter").show();
         } else if (temp < oldTemp) {
-            $("#colder").show();
+            $("#feedback_colder").show();
         } else {
-            $("#neither").show();
+            $("#feedback_neither").show();
         }
         oldTemp = temp;
 
@@ -26,29 +69,8 @@ $(document).ready(function() {
 
         // Never really submit the form
         return false;
-    });
+    }
+
+    initialize();
 });
 
-// Returns a random integer between min and max
-// Using Math.round() will give you a non-uniform distribution!
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-// Transform temperature to height
-function tempToHeight(temp) {
-    var maxHeight = 423;
-    var maxTemp = 100;
-    return temp * maxHeight / maxTemp;
-}
-
-// Get guess temperature
-function getGuessTemp(secretNumber) {
-    var guess_value = $("#guess").val()
-    console.log("guess: "  + guess_value);
-
-    var temp = 100 - Math.abs(secretNumber - guess_value);
-    console.log("temp: " + temp);
-
-    return temp;
-}
