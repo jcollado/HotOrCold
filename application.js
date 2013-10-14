f9ed405de720c0183179309075826beb48b89273
Javier Collado <javier.collado@gmail.com>
2013-10-14 02:06:49 +0200

    Added secret number and form submission code
    For now secret number is just generated and form submision is
    intercepted in a callback to evaluate guess value.

--- application.js
+++ application.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-    console.log("Hello from jquery!");
+    var secretNumber = getRandomInt(0, 100);
+    console.log("secretNumber: " + secretNumber);
+
+    $("form").submit(function() {
+        var guess_value = $("#guess").val()
+        console.log("guess: "  + guess_value);
+        return false;
+    });
 });
+
+// Returns a random integer between min and max
+// Using Math.round() will give you a non-uniform distribution!
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
