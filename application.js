9d37159999c5eb04c2d41f195531717fd83532a1
Javier Collado <javier.collado@gmail.com>
2013-10-14 02:41:30 +0200

    Refactored and added animation to thermometer height

--- application.js
+++ application.js
@@ -3,8 +3,11 @@ $(document).ready(function() {
     console.log("secretNumber: " + secretNumber);
 
     $("form").submit(function() {
-        var guess_value = $("#guess").val()
-        console.log("guess: "  + guess_value);
+        var temp = getGuessTemp(secretNumber);
+
+        var mercury = $("#th-mercury .th-top");
+        mercury.animate({height: tempToHeight(temp)}, "fast");
+
         return false;
     });
 });
@@ -13,4 +16,22 @@ $(document).ready(function() {
 // Using Math.round() will give you a non-uniform distribution!
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// Transform temperature to height
+function tempToHeight(temp) {
+    var maxHeight = 423;
+    var maxTemp = 100;
+    return temp * maxHeight / maxTemp;
+}
+
+// Get guess temperature
+function getGuessTemp(secretNumber) {
+    var guess_value = $("#guess").val()
+    console.log("guess: "  + guess_value);
+
+    var temp = 100 - Math.abs(secretNumber - guess_value);
+    console.log("temp: " + temp);
+
+    return temp;
 }
